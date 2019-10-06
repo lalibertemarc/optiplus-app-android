0c9d3c6f6f38ce80a2be07cb26597efd934cf751
Marc Laliberté <mlaliberte@simaudio.com>
2019-10-06 15:13:40 -0400

    added cheeseknife + basic methods in main activity + started swimmers fragment

---
+++ SwimTimeTracker/Adapters/SwimmerAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Support.V7.Widget;
+using Android.Views;
+using SwimTimeTracker.Models;
+
+namespace SwimTimeTracker.Adapters
+{
+    public class SwimmerAdapter : RecyclerView.Adapter
+    {
+        List<Swimmer> _swimmers;
+
+        public SwimmerAdapter(List<Swimmer> list)
+        {
+            _swimmers = list;
+        }
+
+        public override int ItemCount => _swimmers.Count;
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SwimTimeTracker/MainActivity.cs
+++ SwimTimeTracker/MainActivity.cs
@@ -3,24 +3,72 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Support.Design.Widget;
+using SwimTimeTracker.Helpers;
+using System.Collections.Generic;
+using System;
 
 namespace SwimTimeTracker
 {
     [Activity(Label = "MainActivity", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        [InjectView(Resource.Id.bottom_navigation)]
+        BottomNavigationView _bottomNavigationView;
+
+        [InjectView(Resource.Id.content_frame)]
+        FrameLayout _contentFrame;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            Cheeseknife.Inject(this);
+            SetToolbarInfo("Swim Time Tracker", string.Empty, false);
+            _bottomNavigationView.NavigationItemSelected += BottomNavigationView_NavigationItemSelected;
+
         }
+
+        private void BottomNavigationView_NavigationItemSelected(object sender, BottomNavigationView.NavigationItemSelectedEventArgs e)
+        {
+            switch (e.Item.ItemId)
+            {
+                case Resource.Id.menu_swimmers:
+                    break;
+                case Resource.Id.menu_events:
+                    break;
+                case Resource.Id.menu_time:
+                    break;
+                case Resource.Id.menu_records:
+                    break;
+
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public virtual void SetToolbarInfo(string title, string subTitle, bool showSubTitle)
+        {
+            SupportActionBar.Title = title;
+            if (showSubTitle)
+                SupportActionBar.Subtitle = subTitle;
+        }
+
+        private void LoadFragment(Android.Support.V4.App.Fragment fragment)
+        {
+            // load fragment
+            var transaction = SupportFragmentManager.BeginTransaction();
+            transaction.Replace(Resource.Id.content_frame, fragment);
+            transaction.AddToBackStack(null);
+            transaction.Commit();
         }
     }
 }
